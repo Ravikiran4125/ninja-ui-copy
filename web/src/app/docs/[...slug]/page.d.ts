563ec70e3bb219790500d3052d7a69bbb3bb1eee
ravikiran-pro <52510178+ravikiran-pro@users.noreply.github.com>
2025-06-12 08:28:13 +0530

    Deleted page.d.ts

--- web/src/app/docs/[...slug]/page.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-export declare function generateStaticParams(): Promise<{
-    slug: string[];
-}[]>;
-export default function DocPage({ params }: {
-    params: {
-        slug: string[];
-    };
-}): Promise<import("react").JSX.Element>;
